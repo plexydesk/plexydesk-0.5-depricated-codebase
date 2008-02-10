64871f8b28294dd2022cae1de2e420fb17686c04
sirajr <sirajr@e1f824a7-5b42-0410-8aa0-0b4c0c30c80c>
2008-02-10 23:47:52 +0000

    missing plugin interface

---
+++ widgets/qt4/abstractplugininterface.h
@@ -0,0 +1,23 @@
+#ifndef PLEXY_BASE_PLUGIN_h
+#define PLEXY_BASE_PLUGIN_h
+
+
+#include <plexy.h>
+#include <QtCore>
+#include <QtGui>
+
+namespace PlexyDesk
+{
+    class AbstractPluginInterface 
+    {
+        public:
+           virtual void init() = 0;
+           virtual  QGraphicsItem * backdrop() {return new QGraphicsRectItem ();};
+           virtual  void render(QPainter *p,QRectF r) {};
+
+
+    };
+} // namespace PlexyDesk
+
+Q_DECLARE_INTERFACE(PlexyDesk::AbstractPluginInterface, "org.plexydesk.PluginInterface")
+#endif
