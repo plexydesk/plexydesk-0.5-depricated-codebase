12cb1f388055a246f39f2479df55ab995f197ab0
llahiru <llahiru@e1f824a7-5b42-0410-8aa0-0b4c0c30c80c>
2008-02-29 15:02:38 +0000

    CPU Widget Updated

--- extensions/widgets/cpu/cpu.cpp
+++ extensions/widgets/cpu/cpu.cpp
@@ -16,7 +16,7 @@ Cpu::~Cpu()
 
 QGraphicsItem * Cpu::item()
 {
-return new  PlexyDesk::CpuWidget(QRectF(0,0,340,240),0);
+return new  PlexyDesk::CpuWidget(QRectF(0,0,210,210),0);
 }
 
 Q_EXPORT_PLUGIN2(Cpu,Cpu)
--- extensions/widgets/cpu/cpu.h
+++ extensions/widgets/cpu/cpu.h
@@ -6,12 +6,13 @@
 #include <widgetinterface.h>
 
 
-class VISIBLE_SYM Cpu :public PlexyDesk::WidgetInterface
+class VISIBLE_SYM Cpu :public PlexyDesk::AbstractPluginInterface
 {
     Q_OBJECT
-    Q_INTERFACES(PlexyDesk::WidgetInterface)
+    Q_INTERFACES(PlexyDesk::AbstractPluginInterface)
 
     public:
+    void  init() {} 
     Cpu(QObject * object = 0);
     virtual ~Cpu();
     virtual QGraphicsItem * item();// {};
