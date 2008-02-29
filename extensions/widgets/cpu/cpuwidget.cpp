90a434200a4831b842c3487ba7bbbfd62829b67c
llahiru <llahiru@e1f824a7-5b42-0410-8aa0-0b4c0c30c80c>
2008-02-29 23:38:42 +0000

    Added LGPL3 Header

--- extensions/widgets/cpu/cpuwidget.cpp
+++ extensions/widgets/cpu/cpuwidget.cpp
@@ -1,22 +1,21 @@
-/***************************************************************************
- *   Copyright (C) 2008 by Lahiru Lakmal Priyadarshana                     *
- *   llahiru@gmail.com                                                     *
- *                                                                         *
- *   This program is free software; you can redistribute it and/or modify  *
- *   it under the terms of the GNU General Public License as published by  *
- *   the Free Software Foundation; either version 2 of the License, or     *
- *   (at your option) any later version.                                   *
- *                                                                         *
- *   This program is distributed in the hope that it will be useful,       *
- *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
- *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
- *   GNU General Public License for more details.                          *
- *                                                                         *
- *   You should have received a copy of the GNU General Public License     *
- *   along with this program; if not, write to the                         *
- *   Free Software Foundation, Inc.,                                       *
- *   51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA .        *
- ***************************************************************************/
+/*******************************************************************************
+* This file is part of PlexyDesk.
+*  Maintained by : Siraj Razick <siraj@kde.org>
+*  Authored By  :
+*
+*  PlexyDesk is free software: you can redistribute it and/or modify
+*  it under the terms of the GNU Lesser General Public License as published by
+*  the Free Software Foundation, either version 3 of the License, or
+*  (at your option) any later version.
+*
+*  PlexyDesk is distributed in the hope that it will be useful,
+*  but WITHOUT ANY WARRANTY; without even the implied warranty of
+*  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+*  GNU Lesser General Public License for more details.
+*
+*  You should have received a copy of the GNU General Public License
+*  along with PlexyDesk. If not, see <http://www.gnu.org/licenses/lgpl.html>
+*******************************************************************************/
 
 #include <plexy.h>
 #include "cpuwidget.h"
--- plexycore/qt4/widgetinterface.h
+++ plexycore/qt4/widgetinterface.h
@@ -1,3 +1,21 @@
+/*******************************************************************************
+* This file is part of PlexyDesk.
+*  Maintained by : Siraj Razick <siraj@kde.org>
+*  Authored By  :
+*
+*  PlexyDesk is free software: you can redistribute it and/or modify
+*  it under the terms of the GNU Lesser General Public License as published by
+*  the Free Software Foundation, either version 3 of the License, or
+*  (at your option) any later version.
+*
+*  PlexyDesk is distributed in the hope that it will be useful,
+*  but WITHOUT ANY WARRANTY; without even the implied warranty of
+*  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+*  GNU Lesser General Public License for more details.
+*
+*  You should have received a copy of the GNU General Public License
+*  along with PlexyDesk. If not, see <http://www.gnu.org/licenses/lgpl.html>
+*******************************************************************************/
 
 #ifndef PLEXY_WIDGET_INTERFACE
 #define PLEXY_WIDGET_INTERFACE
