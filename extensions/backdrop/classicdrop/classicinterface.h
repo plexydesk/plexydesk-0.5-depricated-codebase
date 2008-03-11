d74d4b0d478b19a881fa3e06311c8e78f4abab0f
sirajr <sirajr@e1f824a7-5b42-0410-8aa0-0b4c0c30c80c>
2008-03-11 19:30:15 +0000

    more missing files

---
+++ extensions/backdrop/classicdrop/classicinterface.h
@@ -0,0 +1,42 @@
+/*******************************************************************************
+* This file is part of PlexyDesk.
+*  Maintained by : Siraj Razick <siraj@kde.org>
+*  Authored By  :
+*
+*  PlexyDesk is free software: you can redistribute it and/or modify
+*  it under the terms of the GNU Lesser General Public License as published by
+*  the Free Software Foundation, either version 3 of the License, or
+*  (at your option) any later version.
+*
+*  PlexyDesk is distributed in the hope that it will be useful,
+*  but WITHOUT ANY WARRANTY; without even the implied warranty of
+*  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+*  GNU Lesser General Public License for more details.
+*
+*  You should have received a copy of the GNU General Public License
+*  along with PlexyDesk. If not, see <http://www.gnu.org/licenses/lgpl.html>
+*******************************************************************************/
+#ifndef PLEXY_CLASSIC_BACKDROP_I
+#define PLEXY_CLASSIC_BACKDROP_I
+
+#include <QtCore>
+#include <plexy.h>
+#include <backdropinterface.h>
+#include <datainterface.h>
+#include <pluginloader.h>
+#include <abstractplugininterface.h>
+
+
+class VISIBLE_SYM ClassicBackdrop :public PlexyDesk::AbstractPluginInterface 
+{
+    Q_OBJECT
+    Q_INTERFACES(PlexyDesk::AbstractPluginInterface)
+
+    public:
+        ClassicBackdrop(QObject * object = 0);
+        virtual ~ClassicBackdrop();
+        virtual  PlexyDesk::BasePlugin * instance();
+};
+
+
+#endif
