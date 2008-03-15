84a3e564fa846e0620cdbb2a3dd1cb28287fa9c3
sirajr <sirajr@e1f824a7-5b42-0410-8aa0-0b4c0c30c80c>
2008-03-15 03:20:59 +0000

    video interace

---
+++ extensions/data/videoengine/videointerface.cpp
@@ -0,0 +1,36 @@
+/*******************************************************************************
+* This file is part of PlexyDesk.
+*  Maintained by : Siraj Razick <siraj@kde.org>
+*  Authored By  :
+*
+*  PlexyDesk is free software: you can redistribute it and/or modify
+*  it under the terms of the GNU Lesser General Public License as published by
+*  the Free Software Foundation, either version 3 of the License, or
+*  (at your option) any later version.
+*
+*  PlexyDesk is distributed in the hope that it will be useful,
+*  but WITHOUT ANY WARRANTY; without even the implied warranty of
+*  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+*  GNU Lesser General Public License for more details.
+*
+*  You should have received a copy of the GNU General Public License
+*  along with PlexyDesk. If not, see <http://www.gnu.org/licenses/lgpl.html>
+*******************************************************************************/
+#include "flicker.h"
+#include "flickerinterface.h"
+#include <desktopwidget.h>
+#include <plexyconfig.h>
+
+
+    
+FlickerInterface::FlickerInterface(QObject * object)
+{
+}
+
+PlexyDesk::DataPlugin *  FlickerInterface::instance()
+{
+ return new FlickerData();
+}
+
+Q_EXPORT_PLUGIN2(FlickerInterface,FlickerInterface)
+#include "flickerinterface.moc"
