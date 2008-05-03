0a0606ad102f72172a7b71657389df763d0b29f7
rufsketch1 <rufsketch1@e1f824a7-5b42-0410-8aa0-0b4c0c30c80c>
2008-05-03 22:16:32 +0000

    My Second commit

--- playground/java/tester.java
+++ playground/java/tester.java
@@ -1,3 +1,23 @@
+<<<<<<< .mine
+/*******************************************************************************
+* This file is part of PlexyDesk.
+*  Maintained by : Eron Gjoni <egjoni@worcester.edu>
+*  Authored By  : Eron Gjoni
+*
+*  PlexyDesk is free software: you can redistribute it and/or modify
+*  it under the terms of the GNU Lesser General Public License as published by
+*  the Free Software Foundation, either version 3 of the License, or
+*  (at your option) any later version.
+*
+*  PlexyDesk is distributed in the hope that it will be useful,
+*  but WITHOUT ANY WARRANTY; without even the implied warranty of
+*  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+*  GNU Lesser General Public License for more details.
+*
+*  You should have received a copy of the GNU General Public License
+*  along with PlexyDesk. If not, see <http://www.gnu.org/licenses/lgpl.html>
+*******************************************************************************/
+=======
 /*******************************************************************************
 * This file is part of PlexyDesk.
 *  Maintained by : Siraj Razick <siraj@kde.org>
@@ -16,6 +36,7 @@
 *  You should have received a copy of the GNU General Public License
 *  along with PlexyDesk. If not, see <http://www.gnu.org/licenses/lgpl.html>
 *******************************************************************************/
+>>>>>>> .r426
 import java.io.*;
 import java.util.Scanner;
  
