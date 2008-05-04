ebcb91f335569b97e3ce7457b5de881c01761301
sirajr <sirajr@e1f824a7-5b42-0410-8aa0-0b4c0c30c80c>
2008-05-04 19:41:17 +0000

    clean up formating : no code change

--- plexylib/vector3d.h
+++ plexylib/vector3d.h
@@ -16,49 +16,22 @@
 *  You should have received a copy of the GNU General Public License
 *  along with PlexyDesk. If not, see <http://www.gnu.org/licenses/lgpl.html>
 *******************************************************************************/
-/*
- * PlexyDesk
- *
- * Sci-Fi Desktop for the Hurd
- *
- * Authored By Siraj Razick <siraj@kde.org>
- * Authored By Mani Shankar <da.sharp.17@gmail.com> 
- *
- * Copyright (C) 2006 PlexyDeskTeam 
- *
- * This library is free software; you can redistribute it and/or
- * modify it under the terms of the GNU Lesser General Public
- * License as published by the Free Software Foundation; either
- * version 2 of the License, or (at your option) any later version.
- *
- * This library is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- * Lesser General Public License for more details.
- *
- * You should have received a copy of the GNU Lesser General Public
- * License along with this library; if not, write to the
- * Free Software Foundation, Inc., 59 Temple Place - Suite 330,
- * Boston, MA 02111-1307, USA.
- */
 
-#ifndef VECTOR_3D
-#define VECTOR_3D
+#ifndef VECTOR_3D_H
+#define VECTOR_3D_H
 
 #include "plexy.h"
 
 namespace PlexyDesk {
 
-
-	class  VISIBLE_SYM Vector3D 
-	{
+class  VISIBLE_SYM Vector3D 
+{
 	public:
 		
 		/**
 		 * Initialize the vector class, X , Y , and Z is set to 0.0 by default
 		 */
 		Vector3D():x(0.0f),y(0.0f),z(0.0f) {}
-	
 		/**
 		 * Create a user defined Vector
 		 * @param X X Value of the Vector 
@@ -79,21 +52,20 @@ namespace PlexyDesk {
 		Vector3D cross(Vector3D);
 
 		Vector3D unitVector3D();
-
-                /**
-                 * Over loads the - operator there for it lets you subsctract two vectors, as if you are working with 
+         /**
+         * Over loads the - operator there for it lets you subsctract two vectors, as if you are working with 
 		 * any pre-defined data type such as a float, example a = b -c , where a , b , and c are vector objects.
-                 * @param vector 
-                 * @return 
-                 */
-                Vector3D operator-(const Vector3D& vector) const
-                {
-                    return Vector3D(x-vector.x,y-vector.y,z-vector.z);
-                }
+         * @param vector 
+          * @return 
+ 		*/
+         Vector3D operator-(const Vector3D& vector) const
+         {
+           return Vector3D(x-vector.x,y-vector.y,z-vector.z);
+         }
 
-                Vector3D operator+ (const Vector3D& vector) const {
-                    return Vector3D(x+vector.x,y+vector.y,z+vector.z);
-                }
+         Vector3D operator+ (const Vector3D& vector) const {
+             return Vector3D(x+vector.x,y+vector.y,z+vector.z);
+         }
 
                 
 	private:
