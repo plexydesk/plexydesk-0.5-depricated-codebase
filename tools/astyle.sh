796978dbf6e54f8d9d5c70958548e05adb521c18
saenzac <saenzac@e1f824a7-5b42-0410-8aa0-0b4c0c30c80c>
2008-06-02 18:14:02 +0000

    added script for fix *cpp *h with astyle software

---
+++ tools/astyle.sh
@@ -0,0 +1,54 @@
+#!/bin/bash
+
+#script that fix headers and cpp files recursively.
+
+VERBOSE=1
+
+function debug
+{
+  msg=$1
+  if [ "$VERBOSE" == "1" ]; then
+    echo $msg
+  fi
+}
+
+function fix
+{
+  fname=$1
+  nfixedo=0
+  case $(echo ${fname#*.}) in
+    h) debug "[fix header] $fname"
+       astyle $fname > /dev/null
+       nfixedo=`expr $nfixedo + 1`
+       ;;
+    cpp) debug "[fix source] $fname"
+         astyle $fname > /dev/null
+         nfixedo=`expr $nfixedo + 1`
+         ;;
+    *) debug "omiting $fname"
+       ;;
+  esac
+ return ${nfixedo}
+}
+
+function walk
+{
+  for dir in `echo *`; do
+    if [ -d $dir ]; then
+      numdirs=`expr $numdirs + 1`
+      if cd $dir; then
+        debug "[dir] $PWD"
+        walk 
+        cd ..
+      fi
+    elif [ -f $dir ]; then
+      fix $dir
+      nfixed=`expr $nfixed + $?`
+    fi
+  done
+  
+}
+
+walk 0
+echo "Folders visited: $numdirs"
+echo "Files fixed: $nfixed"
