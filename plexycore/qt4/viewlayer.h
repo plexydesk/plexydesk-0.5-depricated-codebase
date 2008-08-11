2405cc8d69966f77d4e577d8e59f2a80426776f3
dariusz.mikulski@gmail.com <dariusz.mikulski@gmail.com@e1f824a7-5b42-0410-8aa0-0b4c0c30c80c>
2008-08-11 15:38:05 +0000

    fix win32 compilation. Again!

--- plexycore/qt4/viewlayer.h
+++ plexycore/qt4/viewlayer.h
@@ -12,32 +12,33 @@
 
 namespace PlexyDesk
 {
-class ViewLayer : public  QObject
-{
-    Q_OBJECT
-public:
-    ViewLayer(QObject* object = 0);
+    class ViewLayer : public  QObject
+    {
+        Q_OBJECT
+    public:
+        ViewLayer(QObject* object = 0);
 
-    virtual ~ViewLayer();
+        virtual ~ViewLayer();
 
-    /** \brief Adds a DesktopWid to a given layer
-     *  \param layerName is is the layer to add the item to, if the
-     *  layer dose not exisit a new layer defined by the layerName is
-     *  created.
-     *  \param item is the widget we wish to add
-     * */
-    void addItem(const QString& layerName , DesktopWidget * item);
+        /** \brief Adds a DesktopWid to a given layer
+        *  \param layerName is is the layer to add the item to, if the
+        *  layer dose not exisit a new layer defined by the layerName is
+        *  created.
+        *  \param item is the widget we wish to add
+        * */
+        void addItem(const QString& layerName , DesktopWidget * item);
 
-    void showLayer(const QString& layer);
+        void showLayer(const QString& layer);
 
-private:
-    typedef QList <DesktopWidget*> List;
-    typedef QMap <QString,List*> Layer;
+    private:
+        typedef QList <DesktopWidget*> List;
+        typedef QMap <QString,List*> Layer;
 
-    Layer layer;
-    List * currentList;
-
-};
+        Layer layer;
+        List * currentList;
+        class Private;
+        Private * const d;
+    };
 } // namespace PlexyDesk
 
 #endif
