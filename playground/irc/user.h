1214d00092db259e90aa561859da7fc11471d19d
da.sharp.17 <da.sharp.17@e1f824a7-5b42-0410-8aa0-0b4c0c30c80c>
2008-10-29 17:24:29 +0000

    forgot to commit user.h in prev commit

---
+++ playground/irc/user.h
@@ -0,0 +1,18 @@
+#ifndef USER_IRC_H
+#define USER_IRC_H
+
+class User
+{
+    public:
+    QString nick;
+    QString user;
+    QString host;
+
+    User(QString nick,QString user, QString host) {
+        this->nick = nick;
+        this->user = user;
+        this->host = host;
+    }
+};
+
+#endif
