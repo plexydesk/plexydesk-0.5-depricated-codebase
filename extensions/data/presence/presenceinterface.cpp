f29e6de6b796d94d1fad8a60f79348c89aab9061
dariusz.mikulski <dariusz.mikulski@e1f824a7-5b42-0410-8aa0-0b4c0c30c80c>
2009-02-07 18:47:16 +0000

    Add presence dataengine

---
+++ extensions/data/presence/presenceinterface.cpp
@@ -0,0 +1,37 @@
+/*******************************************************************************
+* This file is part of PlexyDesk.
+*  Maintained by : Siraj Razick <siraj@kde.org>
+*  Authored By  : Dariusz Mikulski <dariusz.mikulski@gmail.com>
+*
+*  PlexyDesk is free software: you can redistribute it and/or modify
+*  it under the terms of the GNU Lesser General Public License as published by
+*  the Free Software Foundation, either version 3 of the License, or
+*  (at your option) any later version.
+*
+*  PlexyDesk is distributed in the hope that it will be useful,
+*  but WITHOUT ANY WARRANTY; without even the implied warranty of
+*  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+*  GNU Lesser General Public License for more details.
+*
+*  You should have received a copy of the GNU General Public License
+*  along with PlexyDesk. If not, see <http://www.gnu.org/licenses/lgpl.html>
+*******************************************************************************/
+
+#include "presenceinterface.h"
+#include "presence.h"
+
+#include <QtPlugin>
+
+PresenceInterface::PresenceInterface(QObject *parent)
+{
+
+}
+
+PlexyDesk::DataPlugin *PresenceInterface::instance()
+{
+    return new PresenceData();
+}
+
+Q_EXPORT_PLUGIN2(PresenceInterface, PresenceInterface)
+
+
---
+++ extensions/data/presence/presenceinterface.h
@@ -0,0 +1,43 @@
+/*******************************************************************************
+* This file is part of PlexyDesk.
+*  Maintained by : Siraj Razick <siraj@kde.org>
+*  Authored By  : Dariusz Mikulski <dariusz.mikulski@gmail.com>
+*
+*  PlexyDesk is free software: you can redistribute it and/or modify
+*  it under the terms of the GNU Lesser General Public License as published by
+*  the Free Software Foundation, either version 3 of the License, or
+*  (at your option) any later version.
+*
+*  PlexyDesk is distributed in the hope that it will be useful,
+*  but WITHOUT ANY WARRANTY; without even the implied warranty of
+*  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+*  GNU Lesser General Public License for more details.
+*
+*  You should have received a copy of the GNU General Public License
+*  along with PlexyDesk. If not, see <http://www.gnu.org/licenses/lgpl.html>
+*******************************************************************************/
+
+#ifndef PRESENCEINTERFACE_H_
+#define PRESENCEINTERFACE_H_
+
+#include <QObject>
+
+#include <datainterface.h>
+#include <dataplugin.h>
+#include "flicker.h"
+
+class PresenceInterface : public PlexyDesk::DataInterface
+{
+    Q_OBJECT
+    Q_INTERFACES(PlexyDesk::AbstractPluginInterface)
+
+public:
+    PresenceInterface(QObject * object = 0);
+    virtual ~PresenceInterface() {}
+
+    /* this will return a valid data plugin pointer*/
+    PlexyDesk::DataPlugin * instance();
+};
+
+
+#endif //PRESENCEINTERFACE_H_
