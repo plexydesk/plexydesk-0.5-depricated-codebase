ceeaf2d72c2a1bbcde5990f1155144ad83fdb786
Siraj Razick <siraj@debian.(none)>
2009-02-21 15:18:39 +0530

    basic structure class

--- plexywm/compwindow.cpp
+++ plexywm/compwindow.cpp
@@ -1,11 +1,38 @@
 #include "compwindow.h"
 
-CompWindow::CompWindow()
-{
+extern "C" {
+#include <X11/Xutil.h>
+#include <X11/Xatom.h>
+#include <X11/extensions/shape.h>
+#include <X11/cursorfont.h>
+#include <X11/extensions/Xcomposite.h>
+}
+#include <QX11Info>
 
+class CompWindow::Private
+{
+    public:
+        Private(){}
+        ~Private(){}
+        Display* mDisplay;
+        Window  mRootWindow;
+
+        Window mMainWin;
+        Window mMainwinParent;
+        Window mOverlay;
+
+        bool mCompositing;
+        bool mManging;
+
+};
+
+CompWindow::CompWindow():d(new Private)
+{
+    d->mDisplay =  QX11Info::display();
+    d->mRootWindow = QX11Info::appRootWindow();
 }
 
 CompWindow::~CompWindow()
 {
-
+    delete d;
 }
--- plexywm/compwindow.h
+++ plexywm/compwindow.h
@@ -3,14 +3,24 @@
 
 #include <QtCore>
 #include <QtGui>
+
+extern "C" {
+#include <X11/Xlib.h>
+}
+
 class CompWindow : public QWidget
 {
     Q_OBJECT
     public:
         CompWindow();
         ~CompWindow();
+
+        void addWindow(Window id);
     private:
+        void init();//setups  Atoms, registering ..etc
         //data
+        class Private;
+        Private * const d;
 };
 
 #endif
