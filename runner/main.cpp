b34616b28f2c2cba5cd17d0b13f99001d0060bc9
Siraj Razick <siraj@debian.(none)>
2009-02-21 13:15:55 +0530

    disable opengl , intel driver bug

--- runner/main.cpp
+++ runner/main.cpp
@@ -56,7 +56,7 @@ int main( int argc, char ** argv )
     scene.setSceneRect(QDesktopWidget().availableGeometry());//TODO Resolution changes ?
 
     DesktopView *view = new DesktopView(&scene);
-    view->enableOpenGL(true);
+    view->enableOpenGL(false);
     QRect r = QDesktopWidget().availableGeometry();
     view->move(r.x(),r.y());
     view->resize(QDesktopWidget().availableGeometry().size());
