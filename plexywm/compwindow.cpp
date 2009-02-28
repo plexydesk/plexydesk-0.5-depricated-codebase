5dd24a428175800221d46ef11d9ef73f3faf5df4
root <root@debian.(none)>
2009-03-01 01:25:59 +0530

    new register logic

--- plexywm/compwindow.cpp
+++ plexywm/compwindow.cpp
@@ -1,3 +1,21 @@
+/*******************************************************************************
+* This file is part of PlexyDesk.
+*  Maintained by : Siraj Razick <siraj@kde.org>
+*  Authored By  :
+*
+*  PlexyDesk is free software: you can redistribute it and/or modify
+*  it under the terms of the GNU Lesser General Public License as published by
+*  the Free Software Foundation, either version 3 of the License, or
+*  (at your option) any later version.
+*
+*  PlexyDesk is distributed in the hope that it will be useful,
+*  but WITHOUT ANY WARRANTY; without even the implied warranty of
+*  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+*  GNU Lesser General Public License for more details.
+*
+*  You should have received a copy of the GNU General Public License
+*  along with PlexyDesk. If not, see <http://www.gnu.org/licenses/lgpl.html>
+*******************************************************************************/
 #include "compwindow.h"
 
 extern "C" {
@@ -31,12 +49,7 @@ CompWindow::CompWindow():d(new Private)
     d->mDisplay =  QX11Info::display();
     d->mRootWindow = QX11Info::appRootWindow();
     //register
-    if (!isWmRunning()) {
-        registerWindowManager();
-    } else {
-      qDebug()<<"Another Window manager already running.. "<<endl;
-      qApp->quit();
-    }
+    init();
 }
 
 CompWindow::~CompWindow()
@@ -54,17 +67,49 @@ bool CompWindow::isWmRunning()
    return  hasWm;
 }
 
-bool CompWindow::registerWindowManager()
+void CompWindow::init()
 {
-    Atom wmAtom;
+   if (!isWmRunning()) {
+    XSetWindowAttributes attrs;
+    attrs.override_redirect = True;
+    attrs.event_mask = PropertyChangeMask;
+
+    d->mMainWin = XCreateWindow (d->mDisplay,
+                                    d->mRootWindow,
+                                    -100, -100, 1, 1,
+                                    0,
+                                    CopyFromParent,
+                                    CopyFromParent,
+                                    (Visual *)CopyFromParent,
+                                    CWOverrideRedirect | CWEventMask,
+                                    &attrs);
+   Atom wmAtom = XInternAtom(d->mDisplay, "WM_S0", false);
+   if(!registerWindowManager(d->mMainWin, wmAtom)) {
+     qDebug()<<"Register window failed"<<endl;
+   }
+   Xutf8SetWMProperties (d->mDisplay, d->mMainWin, "plexydeskwm",
+			  "plexydeskwm", NULL, 0, NULL, NULL, NULL);
+   Atom cmAtom = XInternAtom(d->mDisplay, "_NET_WM_CM_S0", false);
+   
+   if(!registerWindowManager(d->mMainWin, cmAtom)) {
+     qDebug()<<"Register Compsite failed"<<endl;
+   }
+   
+    } else {
+      qDebug()<<"Another Window manager already running.. "<<endl;
+      qApp->quit();
+    }
+}
+bool CompWindow::registerWindowManager(Window getOwner, Atom wmAtom)
+{
+  /*  Atom wmAtom;
     XSetWindowAttributes attrs;
     attrs.override_redirect = True;
     attrs.event_mask = PropertyChangeMask;
 
     wmAtom = XInternAtom(d->mDisplay, "WM_S0", false);
+    */
     Window  owner = XGetSelectionOwner(d->mDisplay, wmAtom);
-    Window  getOwner  = XCreateWindow(d->mDisplay, d->mRootWindow, -100, -100, 1, 1, 0, CopyFromParent,
-            CopyFromParent, (Visual*) CopyFromParent, CWOverrideRedirect | CWEventMask, & attrs);
 
     if (owner != None)
         XSelectInput (d->mDisplay, owner, StructureNotifyMask);
