b807577b961bb55e061ceb5864448d84f2ec066c
Siraj Razick <siraj@debian.(none)>
2009-03-16 08:07:49 +0530

    add plexywindow class

---
+++ plexywm/plexywindows.cpp
@@ -0,0 +1,218 @@
+/*
+   This library is free software; you can redistribute it and/or
+   modify it under the terms of the GNU Library General Public
+   License version 2 as published by the Free Software Foundation.
+
+   This library is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   Library General Public License for more details.
+
+   You should have received a copy of the GNU Library General Public License
+   along with this library; see the file COPYING.LIB.  If not, write to
+   the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+   Boston, MA 02110-1301, USA.
+*/
+
+#include "plexywindows.h"
+
+PlexyWindows::PlexyWindows(QWidget *parent, Qt::WindowFlags f  ):QWidget(parent, f)
+{
+}
+
+int PlexyWindows::devType() const {
+return QWidget::devType();
+}
+
+void PlexyWindows::setVisible(bool visible) {
+QWidget::setVisible(visible);
+}
+
+QSize PlexyWindows::sizeHint() const {
+return QWidget::sizeHint();
+}
+
+QSize PlexyWindows::minimumSizeHint() const {
+return QWidget::minimumSizeHint();
+}
+
+int PlexyWindows::heightForWidth(int width) const {
+return QWidget::heightForWidth(width);
+}
+
+QPaintEngine* PlexyWindows::paintEngine() const {
+return QWidget::paintEngine();
+}
+
+bool PlexyWindows::event(QEvent* e) {
+return QWidget::event(e);
+}
+
+void PlexyWindows::mousePressEvent(QMouseEvent* e) {
+QWidget::mousePressEvent(e);
+}
+
+void PlexyWindows::mouseReleaseEvent(QMouseEvent* e) {
+QWidget::mouseReleaseEvent(e);
+}
+
+void PlexyWindows::mouseDoubleClickEvent(QMouseEvent* e) {
+QWidget::mouseDoubleClickEvent(e);
+}
+
+void PlexyWindows::mouseMoveEvent(QMouseEvent* e) {
+QWidget::mouseMoveEvent(e);
+}
+
+void PlexyWindows::wheelEvent(QWheelEvent* e) {
+QWidget::wheelEvent(e);
+}
+
+void PlexyWindows::keyPressEvent(QKeyEvent* e) {
+QWidget::keyPressEvent(e);
+}
+
+void PlexyWindows::keyReleaseEvent(QKeyEvent* e) {
+QWidget::keyReleaseEvent(e);
+}
+
+void PlexyWindows::focusInEvent(QFocusEvent* e) {
+QWidget::focusInEvent(e);
+}
+
+void PlexyWindows::focusOutEvent(QFocusEvent* e) {
+QWidget::focusOutEvent(e);
+}
+
+void PlexyWindows::enterEvent(QEvent* e) {
+QWidget::enterEvent(e);
+}
+
+void PlexyWindows::leaveEvent(QEvent* e) {
+QWidget::leaveEvent(e);
+}
+
+void PlexyWindows::paintEvent(QPaintEvent* e) {
+QWidget::paintEvent(e);
+}
+
+void PlexyWindows::moveEvent(QMoveEvent* e) {
+QWidget::moveEvent(e);
+}
+
+void PlexyWindows::resizeEvent(QResizeEvent* e) {
+QWidget::resizeEvent(e);
+}
+
+void PlexyWindows::closeEvent(QCloseEvent* e) {
+QWidget::closeEvent(e);
+}
+
+void PlexyWindows::contextMenuEvent(QContextMenuEvent* e) {
+QWidget::contextMenuEvent(e);
+}
+
+void PlexyWindows::tabletEvent(QTabletEvent* e) {
+QWidget::tabletEvent(e);
+}
+
+void PlexyWindows::actionEvent(QActionEvent* e) {
+QWidget::actionEvent(e);
+}
+
+void PlexyWindows::dragEnterEvent(QDragEnterEvent* e) {
+QWidget::dragEnterEvent(e);
+}
+
+void PlexyWindows::dragMoveEvent(QDragMoveEvent* e) {
+QWidget::dragMoveEvent(e);
+}
+
+void PlexyWindows::dragLeaveEvent(QDragLeaveEvent* e) {
+QWidget::dragLeaveEvent(e);
+}
+
+void PlexyWindows::dropEvent(QDropEvent* e) {
+QWidget::dropEvent(e);
+}
+
+void PlexyWindows::showEvent(QShowEvent*e ) {
+QWidget::showEvent(e);
+}
+
+void PlexyWindows::hideEvent(QHideEvent* e) {
+QWidget::hideEvent(e);
+}
+
+bool PlexyWindows::x11Event(_XEvent* e) {
+return QWidget::x11Event(e);
+}
+
+void PlexyWindows::changeEvent(QEvent*e ) {
+QWidget::changeEvent(e);
+}
+
+int PlexyWindows::metric(QPaintDevice::PaintDeviceMetric m ) const {
+return QWidget::metric(m);
+}
+
+void PlexyWindows::inputMethodEvent(QInputMethodEvent* e) {
+QWidget::inputMethodEvent(e);
+}
+
+QVariant PlexyWindows::inputMethodQuery(Qt::InputMethodQuery q) const {
+return QWidget::inputMethodQuery(q);
+}
+
+bool PlexyWindows::focusNextPrevChild(bool next) {
+return QWidget::focusNextPrevChild(next);
+}
+
+void PlexyWindows::styleChange(QStyle& s) {
+QWidget::styleChange(s);
+}
+
+void PlexyWindows::enabledChange(bool b) {
+QWidget::enabledChange(b);
+}
+
+void PlexyWindows::paletteChange(const QPalette& p) {
+QWidget::paletteChange(p);
+}
+
+void PlexyWindows::fontChange(const QFont& f) {
+QWidget::fontChange(f);
+}
+
+void PlexyWindows::windowActivationChange(bool b) {
+QWidget::windowActivationChange(b);
+}
+
+void PlexyWindows::languageChange() {
+QWidget::languageChange();
+}
+
+bool PlexyWindows::eventFilter(QObject* o , QEvent* e) {
+return QObject::eventFilter(o, e);
+}
+
+void PlexyWindows::timerEvent(QTimerEvent* e) {
+QObject::timerEvent(e);
+}
+
+void PlexyWindows::childEvent(QChildEvent* e) {
+QObject::childEvent(e);
+}
+
+void PlexyWindows::customEvent(QEvent*e ) {
+QObject::customEvent(e);
+}
+
+void PlexyWindows::connectNotify(const char* signal) {
+QObject::connectNotify(signal);
+}
+
+void PlexyWindows::disconnectNotify(const char* signal) {
+QObject::disconnectNotify(signal);
+}
+
---
+++ plexywm/plexywindows.h
@@ -0,0 +1,92 @@
+/*
+   This library is free software; you can redistribute it and/or
+   modify it under the terms of the GNU Library General Public
+   License version 2 as published by the Free Software Foundation.
+
+   This library is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   Library General Public License for more details.
+
+   You should have received a copy of the GNU Library General Public License
+   along with this library; see the file COPYING.LIB.  If not, write to
+   the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+   Boston, MA 02110-1301, USA.
+*/
+
+#ifndef PLEXYWINDOWS_H
+#define PLEXYWINDOWS_H
+
+#include <QtCore>
+#include <QtGui>
+#include <QWidget>
+
+
+class PlexyWindows : public QWidget
+{
+  Q_OBJECT
+public:
+  PlexyWindows(QWidget * parent = 0, Qt::WindowFlags f = 0);
+virtual ~PlexyWindows(){}
+virtual int devType() const;
+virtual void setVisible(bool visible);
+virtual QSize sizeHint() const;
+virtual QSize minimumSizeHint() const;
+virtual int heightForWidth(int ) const;
+virtual QPaintEngine* paintEngine() const;
+
+protected:
+virtual bool event(QEvent* );
+virtual void mousePressEvent(QMouseEvent* );
+virtual void mouseReleaseEvent(QMouseEvent* );
+virtual void mouseDoubleClickEvent(QMouseEvent* );
+virtual void mouseMoveEvent(QMouseEvent* );
+virtual void wheelEvent(QWheelEvent* );
+virtual void keyPressEvent(QKeyEvent* );
+virtual void keyReleaseEvent(QKeyEvent* );
+virtual void focusInEvent(QFocusEvent* );
+virtual void focusOutEvent(QFocusEvent* );
+virtual void enterEvent(QEvent* );
+virtual void leaveEvent(QEvent* );
+virtual void paintEvent(QPaintEvent* );
+virtual void moveEvent(QMoveEvent* );
+virtual void resizeEvent(QResizeEvent* );
+virtual void closeEvent(QCloseEvent* );
+virtual void contextMenuEvent(QContextMenuEvent* );
+virtual void tabletEvent(QTabletEvent* );
+virtual void actionEvent(QActionEvent* );
+virtual void dragEnterEvent(QDragEnterEvent* );
+virtual void dragMoveEvent(QDragMoveEvent* );
+virtual void dragLeaveEvent(QDragLeaveEvent* );
+virtual void dropEvent(QDropEvent* );
+virtual void showEvent(QShowEvent* );
+virtual void hideEvent(QHideEvent* );
+virtual bool x11Event(_XEvent* );
+virtual void changeEvent(QEvent* );
+virtual int metric(QPaintDevice::PaintDeviceMetric ) const;
+virtual void inputMethodEvent(QInputMethodEvent* );
+
+public:
+virtual QVariant inputMethodQuery(Qt::InputMethodQuery ) const;
+
+protected:
+virtual bool focusNextPrevChild(bool next);
+virtual void styleChange(QStyle& );
+virtual void enabledChange(bool );
+virtual void paletteChange(const QPalette& );
+virtual void fontChange(const QFont& );
+virtual void windowActivationChange(bool );
+virtual void languageChange();
+
+public:
+virtual bool eventFilter(QObject* , QEvent* );
+
+protected:
+virtual void timerEvent(QTimerEvent* );
+virtual void childEvent(QChildEvent* );
+virtual void customEvent(QEvent* );
+virtual void connectNotify(const char* signal);
+virtual void disconnectNotify(const char* signal);
+};
+
+#endif // PLEXYWINDOWS_H
