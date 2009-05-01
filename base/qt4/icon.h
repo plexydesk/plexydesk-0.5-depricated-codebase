d93e79b22ef154f9fe141710102a13618f407b8e
Siraj Razick <siraj.razick@collabora.co.uk>
2009-05-02 02:05:36 +0530

    Add Icon class

---
+++ base/qt4/icon.h
@@ -0,0 +1,39 @@
+/*
+   This library is free software; you can redistribute it and/or
+   modify it under the terms of the GNU Library General Public
+   License version 2 as published by the Free Software Foundation.
+
+   This library is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   Library General Public License for more details.
+
+   You should have received a copy of the GNU Library General Public License
+   along with this library; see the file COPYING.LIB.  If not, write to
+   the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+   Boston, MA 02110-1301, USA.
+*/
+
+#ifndef ICON_H
+#define ICON_H
+
+#include <QGraphicsRectItem>
+
+#include <plexy.h>
+#include <plexyconfig.h>
+#include <desktopwidget.h>
+namespace PlexyDesk
+{
+   class VISIBLE_SYM Icon : public DesktopWidget
+   {
+       Q_OBJECT
+   public:
+       Icon(const QRectF &rect, QWidget *embeddedWidget = 0);
+       virtual ~Icon();
+       virtual void paintBackSide(QPainter * painter,const QRectF& rect);
+       virtual void paintViewSide(QPainter * painter,const QRectF& rect);
+       virtual void paintDockView(QPainter * painter,const QRectF& rect);
+
+    };
+}
+#endif // ICON_H
