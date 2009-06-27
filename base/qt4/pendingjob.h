b5cb4baae7d97f1750cc9eb13bfc7617b95315b9
Siraj Razick <siraj.razick@collabora.co.uk>
2009-06-27 12:45:37 +0530

    some comments and clean up of code

--- base/qt4/pendingjob.h
+++ base/qt4/pendingjob.h
@@ -6,14 +6,29 @@
 
 namespace PlexyDesk
 {
+/** This class is closely based on PendingOperation class  from telepathy Qt4, 
+which in turn inherits features from KJob from KDE project. This class is intended 
+to provide similar functions to plexydesk
+
+When modeling asynchronous classes try to inherit from this class.
+
+**/
 class PendingJob : public QObject
 {
     Q_OBJECT
 public:
         Q_DISABLE_COPY(PendingJob)
+        /** 
+        **/ 
         bool isError();
+        /**
+        **/
         QString errorName() const;
+        /**
+        **/
         QString errorMessage() const;
+        /**
+        **/
         void setFinished(bool status, QString &error, const QString &message);
 Q_SIGNALS:
     void finished();
