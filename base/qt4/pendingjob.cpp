cd126bec0be7604ab41cd27aa04992f63b4273ee
Siraj Razick <siraj.razick@collabora.co.uk>
2009-06-27 10:35:17 +0530

    Pending Job class

---
+++ base/qt4/pendingjob.cpp
@@ -0,0 +1,43 @@
+#include "pendingjob.h"
+
+namespace PlexyDesk
+{
+class PendingJob::Private {
+    public:
+    Private(){}
+    ~Private(){}
+    bool done;
+    bool iserror;
+    QString error;
+    QString message;
+};
+
+PendingJob::PendingJob(QObject* parent):QObject(parent), d(new Private)
+{
+    d->done = false;
+    d->iserror = false;
+}
+
+bool PendingJob::isError()
+{
+   return d->iserror;
+}
+
+QString PendingJob::errorName() const
+{
+    return d->error;
+}
+
+QString PendingJob::errorMessage() const
+{
+   return d->message;
+}
+
+void PendingJob::setFinished(bool status, QString &error, const QString &message)
+{
+    d->done  = status;
+    d->error = error;
+    d->message = message;
+    emit finished();
+}
+}
---
+++ base/qt4/pendingjob.h
@@ -0,0 +1,27 @@
+#ifndef PENDINGJOB_H
+#define PENDINGJOB_H
+#include <plexy.h>
+#include <QtCore>
+
+
+namespace PlexyDesk
+{
+class PendingJob : public QObject
+{
+    Q_OBJECT
+public:
+        Q_DISABLE_COPY(PendingJob)
+        bool isError();
+        QString errorName() const;
+        QString errorMessage() const;
+        void setFinished(bool status, QString &error, const QString &message);
+Q_SIGNALS:
+    void finished();
+protected:
+      PendingJob(QObject* parent);
+private:
+       class Private;
+       Private * const d;
+};
+}
+#endif // PENDINGJOB_H
