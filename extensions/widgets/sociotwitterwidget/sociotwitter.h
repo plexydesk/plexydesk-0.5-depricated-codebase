f02d5d000c71f6fe3aacbbc617517e052dc29234
Siraj Razick <siraj.razick@collabora.co.uk>
2009-08-22 08:37:09 -0400

    Socioplexy Twitter Widget

---
+++ extensions/widgets/sociotwitterwidget/sociotwitter.h
@@ -0,0 +1,20 @@
+#ifndef SOCIOTWITTER_H
+#define SOCIOTWITTER_H
+
+#include <plexy.h>
+#include <widgetinterface.h>
+#include <widgetplugin.h>
+#include "sociotwitterinterface.h"
+
+class VISIBLE_SYM SocioTwitter:public PlexyDesk::WidgetPlugin
+{
+    Q_OBJECT
+
+    public:
+        SocioTwitter(QObject * object = 0);
+        virtual ~SocioTwitter();
+        QGraphicsItem * item();
+};
+
+#endif
+
