02fe9796913444489c5300191253724622048e62
Siraj Razick <siraj.razick@collabora.co.uk>
2009-09-20 08:11:50 +0530

    restart coding workon plexydesk window manager   * initial clean up of the plexywindow class, it s   no longer a qwidget

--- plexywm/plexywindows.h
+++ plexywm/plexywindows.h
@@ -26,71 +26,20 @@ extern "C" {
 #include <X11/extensions/Xdamage.h>
 }
 
-class PlexyWindows : public QWidget
+class PlexyWindows : public QObject
 {
   Q_OBJECT
 public:
   PlexyWindows(Display*d, Window w, XWindowAttributes *attr, QWidget * parent = 0, Qt::WindowFlags f = 0);
 virtual ~PlexyWindows(){}
-virtual int devType() const;
-virtual void setVisible(bool visible);
-virtual QSize sizeHint() const;
-virtual QSize minimumSizeHint() const;
-virtual int heightForWidth(int ) const;
-virtual QPaintEngine* paintEngine() const;
 
-protected:
-virtual bool event(QEvent* );
-virtual void mousePressEvent(QMouseEvent* );
-virtual void mouseReleaseEvent(QMouseEvent* );
-virtual void mouseDoubleClickEvent(QMouseEvent* );
-virtual void mouseMoveEvent(QMouseEvent* );
-virtual void wheelEvent(QWheelEvent* );
-virtual void keyPressEvent(QKeyEvent* );
-virtual void keyReleaseEvent(QKeyEvent* );
-virtual void focusInEvent(QFocusEvent* );
-virtual void focusOutEvent(QFocusEvent* );
-virtual void enterEvent(QEvent* );
-virtual void leaveEvent(QEvent* );
-virtual void paintEvent(QPaintEvent* );
-virtual void moveEvent(QMoveEvent* );
-virtual void resizeEvent(QResizeEvent* );
-virtual void closeEvent(QCloseEvent* );
-virtual void contextMenuEvent(QContextMenuEvent* );
-virtual void tabletEvent(QTabletEvent* );
-virtual void actionEvent(QActionEvent* );
-virtual void dragEnterEvent(QDragEnterEvent* );
-virtual void dragMoveEvent(QDragMoveEvent* );
-virtual void dragLeaveEvent(QDragLeaveEvent* );
-virtual void dropEvent(QDropEvent* );
-virtual void showEvent(QShowEvent* );
-virtual void hideEvent(QHideEvent* );
-virtual bool x11Event(_XEvent* );
-virtual void changeEvent(QEvent* );
-virtual int metric(QPaintDevice::PaintDeviceMetric ) const;
-virtual void inputMethodEvent(QInputMethodEvent* );
+void Destroyed ();
+void Mapped (bool override_redirect);
+void Unmapped ();
+ void PropertyChanged (Atom prop, bool deleted);
 
-public:
-virtual QVariant inputMethodQuery(Qt::InputMethodQuery ) const;
 
-protected:
-virtual bool focusNextPrevChild(bool next);
-virtual void styleChange(QStyle& );
-virtual void enabledChange(bool );
-virtual void paletteChange(const QPalette& );
-virtual void fontChange(const QFont& );
-virtual void windowActivationChange(bool );
-virtual void languageChange();
 
-public:
-virtual bool eventFilter(QObject* , QEvent* );
-
-protected:
-virtual void timerEvent(QTimerEvent* );
-virtual void childEvent(QChildEvent* );
-virtual void customEvent(QEvent* );
-virtual void connectNotify(const char* signal);
-virtual void disconnectNotify(const char* signal);
 };
 
 #endif // PLEXYWINDOWS_H
