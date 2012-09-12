91c6a336d56619180a9b87cb253513eee49a38e9
Siraj Razick <siraj@kde.org>
2012-09-11 23:15:38 -0400

    [desktop] Set the default desktop view properties
    Remves scroolbars and make the background image visible

--- extensions/desktop/plexy/plexydesktopview.cpp
+++ extensions/desktop/plexy/plexydesktopview.cpp
@@ -20,6 +20,11 @@ PlexyDesktopView::PlexyDesktopView(QGraphicsScene *scene, QWidget *parent) :
     QGraphicsView(scene, parent),
     d(new PrivatePlexyDesktopView)
 {
+    setWindowFlags(Qt::FramelessWindowHint | Qt::WindowStaysOnBottomHint);
+    setHorizontalScrollBarPolicy(Qt::ScrollBarAlwaysOff);
+    setVerticalScrollBarPolicy(Qt::ScrollBarAlwaysOff);
+    setAlignment(Qt::AlignLeft | Qt::AlignTop);
+
     d->mBackgroundItem = 0;
     d->mBackgroundSource = 0;
     setBackgroundSource("classicbackdrop");
@@ -28,16 +33,30 @@ PlexyDesktopView::PlexyDesktopView(QGraphicsScene *scene, QWidget *parent) :
 bool PlexyDesktopView::setBackgroundSource(const QString &source_id)
 {
 
+    //TODO: error handling
+    // delete the current background source before setting a new one
+    if (d->mBackgroundSource)
+        return 0;
+
     d->mBackgroundSource =
             qobject_cast<PlexyDesk::BackgroundSource*> (PlexyDesk::PluginLoader::getInstance()->instance(source_id));
 
     if (!d->mBackgroundSource)
         return 0;
 
+    QRectF screenRect = scene()->sceneRect();
+    qDebug() << Q_FUNC_INFO << screenRect;
+    d->mBackgroundSource->setRect (
+                QRect (screenRect.x(),
+                       screenRect.y(),
+                       screenRect.width(),
+                       screenRect.height()));
 
     d->mBackgroundItem = d->mBackgroundSource->item();
 
     scene()->addItem(d->mBackgroundItem);
+
+    d->mBackgroundItem->show();
 
     return true;
 }
