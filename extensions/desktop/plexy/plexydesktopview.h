91220bbc7cd47626c5dca2e71287545ce252ac67
Siraj Razick <siraj@plexyplanet.org>
2012-09-16 04:28:29 -0400

    [runtime] Clean up Desktop UI setup
    Just a clean up to remove all the duplicate code from the
    desktop ui setup method. Also update to match the new API
    changes.

--- extensions/desktop/plexy/plexydesktopview.h
+++ extensions/desktop/plexy/plexydesktopview.h
@@ -2,19 +2,18 @@
 #define PLEXYDESKTOPVIEW_H
 
 #include <QGraphicsView>
+#include <abstractdesktopview.h>
 
-class PlexyDesktopView : public QGraphicsView
+class PlexyDesktopView : public PlexyDesk::AbstractDesktopView
 {
     Q_OBJECT
 public:
     explicit PlexyDesktopView(QGraphicsScene *parent_scene, QWidget *parent = 0);
 
-    bool setBackgroundSource(const QString& source_id);
-
 protected:
     virtual void dropEvent(QDropEvent *event);
-    virtual void dragEnterEvent (QDragEnterEvent * event);
-    virtual void dragMoveEvent (QDragMoveEvent * event);
+    virtual void dragEnterEvent (QDragEnterEvent *event);
+    virtual void dragMoveEvent (QDragMoveEvent *event);
 
 private:
     class PrivatePlexyDesktopView;
