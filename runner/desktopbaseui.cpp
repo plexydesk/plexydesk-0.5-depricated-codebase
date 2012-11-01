29c12ee8877c343221f290f7d47d3a678cd15f8b
Siraj Razick <siraj@kde.org>
2012-11-01 03:45:00 -0400

    [runtime] Remove Duplicate setup method

--- runner/desktopbaseui.cpp
+++ runner/desktopbaseui.cpp
@@ -54,7 +54,7 @@ DesktopBaseUi::DesktopBaseUi(QWidget *parent) :
     d (new DesktopBaseUiPrivate)
 {
     setDesktopView(QLatin1String ("plexydesktopview"));
-    setup_single();
+    setup();
 
     connect (d->mDesktopWidget, SIGNAL(resized(int)), this, SLOT(screenResized(int)));
 }
@@ -76,7 +76,7 @@ void DesktopBaseUi::setDesktopView(const QString &name)
     //FIX : handle changes
 }
 
-void DesktopBaseUi::setup_single()
+void DesktopBaseUi::setup()
 {
     d->mDesktopWidget = new QDesktopWidget ();
     d->mConfig = PlexyDesk::Config::getInstance();
@@ -133,74 +133,6 @@ void DesktopBaseUi::setup_single()
     QApplication::desktop()->setParent(view);
 
     view->layout(desktopRect());
-}
-
-void DesktopBaseUi::setup()
-{
-    d->mDesktopWidget = new QDesktopWidget ();
-    d->mConfig = PlexyDesk::Config::getInstance();
-    QSize desktopSize = (QSize) desktopRect().size();
-
-    QGraphicsScene *scene = new QGraphicsScene;
-    d->mScene = scene;
-
-    for (int i = 0 ; i < d->mDesktopWidget->screenCount() ; i++ ) {
-        QRect desktopScreenRect = d->mDesktopWidget->screenGeometry(i);
-#ifdef Q_WS_WIN
-        // A 1px hack to make the widget fullscreen and not covering the toolbar on Win
-        desktopScreenRect.setHeight(desktopScreenRect.height()-1);
-#endif
-        desktopSize = d->mDesktopWidget->screenGeometry(i).size();
-
-        if (!d->mViewPlugin) {
-            continue;
-        }
-
-        QObject *viewPlugin = d->mViewPlugin->view(scene);
-        PlexyDesk::AbstractDesktopView *view = qobject_cast<PlexyDesk::AbstractDesktopView *> (viewPlugin);
-
-        if (!view) {
-            continue;
-        }
-
-        view->resize(desktopSize);
-        view->move(d->mDesktopWidget->screenGeometry(i).x(),
-                  d->mDesktopWidget->screenGeometry(i).y());
-
-        view->setSceneRect (desktopScreenRect);
-
-        view->setDragMode(QGraphicsView::RubberBandDrag);
-
-#ifdef Q_WS_X11
-        NETWinInfo info(QX11Info::display(), view->winId(), QX11Info::appRootWindow(), NET::WMDesktop );
-        info.setDesktop(NETWinInfo::OnAllDesktops);
-        info.setWindowType(NET::Desktop);
-#endif
-        //view->showLayer(QLatin1String("Widgets"));
-        d->mViewList[i] = view;
-        QWidget *parentWidget = qobject_cast<QWidget*>(parent());
-        if(parentWidget) {
-            this->resize(view->size());
-            view->setParent(this);
-
-#ifdef Q_WS_MAC
-            //TODO: until we write our own NSView we do this for mac (issue : 169)
-            /*- (void)drawRect:(NSRect)rect {
-                [[NSColor clearColor] set];
-                NSRectFill(rect);
-              }
-           */
-            view->setStyleSheet("background-color: transparent;");
-#endif
-        }
-        view->show();
-#ifdef PLEXYNAME
-        view->setWindowTitle(QString(PLEXYNAME));
-#endif
-        QApplication::desktop()->setParent(view);
-
-        view->layout(desktopScreenRect);
-    }
 }
 
 void DesktopBaseUi::screenResized(int screen)
